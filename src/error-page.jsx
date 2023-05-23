9f3b759ba1cba1945e75ed54ab492a65084adb6c
Ana <ana.pinheiro@telecall.com>
2023-05-23 13:04:49 -0300

    update

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,9 +1,4 @@
 import { useRouteError } from "react-router-dom";
-<<<<<<< HEAD
-
-=======
-import imagemLogo from "../src/assets/imgs/erro.jpg"
->>>>>>> 8d889c6394564da19c11637305e36c4ac69974e8
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -13,13 +8,9 @@ export default function ErrorPage() {
     <div id="error-page" className=" flex h-screen bg-aurora-fundoErro justify-center items-center">
       <div className=" "  >
         <h1 className="flex pb-1 justify-center items-center text-2xl text-aurora-fontErroRosa font-bold">NOT FOUND</h1>
-<<<<<<< HEAD
         {/* <img src={imagemLogo} alt=""  className="flex h-96 "/> */}
 
         
-=======
-        <img src={imagemLogo} alt=""  className="flex h-96 "/>
->>>>>>> 8d889c6394564da19c11637305e36c4ac69974e8
       </div>
       
     </div>
